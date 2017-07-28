e29425d47d03285f52fcec76623dd8bcc63152fa
Breyana Scales <breyanaj@gmail.com>
2017-07-28 16:10:53 -0700

    Move response.on 'end' functionality into separate function

--- movie-search.js
+++ movie-search.js
@@ -4,25 +4,29 @@ const cheerio = require('cheerio')
 const search = encodeURI(process.argv[2])
 const URL = 'http://www.imdb.com/find?ref_=nv_sr_fn&q=' + search + '&s=all'
 
+const printTitles = (rawData) => {
+  const $ = cheerio.load(rawData)
+  const titles = []
+  //There are multiple sections called .findSection, we only need the movies
+  //so we look for the first .findSection, and grab the .result_text from it
+  $('.findSection')
+    .first()
+    .find('.result_text')
+    .each((i, elem) => {
+      titles[i] = $(elem).text()
+    })
+  titles.forEach((element) => {
+    console.log(element)
+  })
+}
+
 http.get(URL, (response) => {
   response.setEncoding('utf8')
   let rawData = ''
   response.on('data', (data) => { rawData += data })
   response.on('end', () => {
     try {
-      const $ = cheerio.load(rawData)
-      const titles = []
-      //There are multiple sections called .findSection, we only need the movies
-      //so we look for the first .findSection, and grab the .result_text from it
-      $('.findSection')
-        .first()
-        .find('.result_text')
-        .each((i, elem) => {
-          titles[i] = $(elem).text()
-        })
-      titles.forEach((element) => {
-        console.log(element)
-      })
+      printTitles(rawData)
     } catch (error) {
       console.error(error)
     }
